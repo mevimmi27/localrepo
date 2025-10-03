a58535dd2f81dd98938c7538c9570b39b57a5552
unknown <vimmi.singh07@gmail.com>
2025-10-03 22:32:23 +0530

    My first commit

---
+++ src/com/test/gitBasics.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class gitBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a= 5;
+		int b= 4;
+		int sum= a+b;
+		System.out.println("The sum of a & b = " + sum);
+		
+		System.out.println("Hello I am Git");
+
+	}
+
+}
